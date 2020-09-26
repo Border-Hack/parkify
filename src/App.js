02095bed0968a36615cdea6bd6a81e2f7259d418
Divyam Tayal <daretobedifferent10920@gmail.com>
2020-09-26 23:08:39 +0530

    Created Login Page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Layout from "./components/Layout/Layout";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Login from "./components/Login/Login";
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <Layout>
+      <Route path="/login" exact component={Login} />
       <Route path="/about" exact component={About} />
       <Route path="/" exact component={Home} />
     </Layout>
---
+++ src/components/Login/Login.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import styles from "./Login.module.css";
+
+class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
+  onChangeEmailHandler = (event) => {
+    this.setState({ email: event.target.value });
+  };
+  onChangePasswordHandler = (event) => {
+    this.setState({ password: event.target.value });
+  };
+  render() {
+    return (
+      <div className={styles.login}>
+        <div className="container">
+          <div className="col-xs-12  col-sm-6 col-sm-offset-3">
+            <div className="panel">
+              <div className="panel-heading">
+                <center>
+                  <h4>Login</h4>
+                </center>
+              </div>
+              <div className="panel-body">
+                <form action="#" method="POST">
+                  <div className="form-group">
+                    <label for="email">Email:</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Email"
+                      id="email"
+                      pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                      required
+                      name="email"
+                      onChange={this.onChangeEmailHandler}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="password">Password:</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      placeholder="Password"
+                      id="password"
+                      minlength="6"
+                      required
+                      name="password"
+                      onChange={this.onChangePasswordHandler}
+                    />
+                  </div>
+                  <button className="btn btn-block">Login</button>
+                </form>
+              </div>
+              <div className="panel-footer">
+                <div>
+                  Don't have an account?
+                  <submit className="btn">Click here to Sign Up</submit>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
